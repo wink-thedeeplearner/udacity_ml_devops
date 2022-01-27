6f2b7b84a5508535e0dfee5b2ca79b914af43628
wink-thedeeplearner <khai.win2018@gmail.com>
2022-01-26 23:50:05 -0500

    add pytests for row_counts & price_range

--- project_2_reproducible_model_workflow/MLPipeline_NYC_Rental_Price_Predictor/src/data_check/test_data.py
+++ project_2_reproducible_model_workflow/MLPipeline_NYC_Rental_Price_Predictor/src/data_check/test_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import logging
 import scipy.stats
 
 
@@ -26,6 +27,8 @@ def test_column_names(data):
 
     these_columns = data.columns.values
 
+    #logging.info("length of these is %s", len(these_columns))
+
     # This also enforces the same order
     assert list(expected_colums) == list(these_columns)
 
@@ -44,7 +47,11 @@ def test_proper_boundaries(data: pd.DataFrame):
     """
     Test proper longitude and latitude boundaries for properties in and around NYC
     """
-    idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.5, 41.2)
+    idx = data['longitude'].between(-74.25, -
+                                    73.50) & data['latitude'].between(40.5, 41.2)
+
+    logging.info(
+        "Test proper boundaries: unexpected items are %s", np.sum(~idx))
 
     assert np.sum(~idx) == 0
 
@@ -60,6 +67,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    """
+    Test row count is into a good range
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    """
+    Check price range is between boundaries
+    """
+    assert data['price'].between(min_price, max_price).all()
